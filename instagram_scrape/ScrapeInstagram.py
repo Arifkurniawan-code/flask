07c4f442d62f8f998f60036eb779c725599464cb
ixvikjh <arifkurniawan96@outlook.com>
2020-06-21 20:09:01 +0700

    a

--- instagram_scrape/ScrapeInstagram.py
+++ instagram_scrape/ScrapeInstagram.py
@@ -25,7 +25,7 @@ class go_url:
     def driver_path(self):
         options = webdriver.ChromeOptions()
         options.add_argument("--headless")
-        options.add_argument('--disable-gpu')
+        options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--no-sandbox')
         options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
         self.driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=options)
